03b841cd871768a6cb537f226d4b2244318ce2ef
yonekawak <d3112101@stu.kogakkan-u.ac.jp>
2022-03-22 20:47:43 +0900

    学習ページ

--- resources/views/studies/index.blade.php
+++ resources/views/studies/index.blade.php
@@ -14,8 +14,8 @@
     <body>
         <h1>学習画面</h1>
         @foreach ($quizzes as $quiz )
-            <p>{{ $quiz[0]->question }}</p>
-            <p>{{ $quiz[1] }}</p>
+            間違えた問題：<p>{{ $quiz[0]->question }}</p>
+            間違えた回数<p>{{ $quiz[1] }}</p>
             
         @endforeach
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
